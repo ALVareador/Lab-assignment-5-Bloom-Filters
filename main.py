89dd97a9738d185f5e396b9c1f5836d5098a7ea9
Álvaro <95368497+ALVareador@users.noreply.github.com>
2023-11-22 16:03:53 +0100

    Cosas

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from BloomFilter import BloomFilter as BF
 import pandas as pd
 import random
 
-predataset = pd.read_csv("pprl-attack-data\german-names.csv")
-dataset = predataset.transpose()
+dataset = pd.read_csv("german-names.csv")
 size = 1000 
 nHashes = 5
 qGrams = 2
 
 filter = BF(dataset, size, nHashes, qGrams)
 
-for sample in dataset.sample(n=random.randint(5, 50), axis=0):
-    filter.add(sample)
+sampleD = dataset.sample(n=random.randint(5, 50), axis=0).transpose()
+
+for sample in sampleD:
+    filter.add(str(sample).encode())
     print(sample)
